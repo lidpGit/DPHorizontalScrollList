6067e6ef4db800d37b913084c732fcf521f01364
boombox <boombox@zzj.local>
2015-08-20 13:09:53 +0800

    Initial Commit

---
+++ DPHorizontalScrollList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DPHorizontalScrollList
+//
+//  Created by boombox on 15/8/20.
+//  Copyright (c) 2015年 lidaipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DPHorizontalScrollList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DPHorizontalScrollList
+//
+//  Created by boombox on 15/8/20.
+//  Copyright (c) 2015年 lidaipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
