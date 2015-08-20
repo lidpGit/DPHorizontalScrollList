71540e6d371995928921de3d8d6b2f98e9287cb5
lidpGit <boombox@zzj.local>
2015-08-20 13:53:00 +0800

    横向滚动列表

--- DPHorizontalScrollList/ViewController.m
+++ DPHorizontalScrollList/ViewController.m
@@ -7,21 +7,43 @@
 //
 
 #import "ViewController.h"
+#import "DPHorizontalScrollView.h"
 
-@interface ViewController ()
+@interface ViewController ()<DPHorizontalScrollViewDelegate>
 
 @end
 
 @implementation ViewController
 
+#pragma mark - ---------------------- override
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    DPHorizontalScrollView *horizontalScrollView = [[DPHorizontalScrollView alloc] initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 350)];
+    horizontalScrollView.scrollViewDelegate = self;
+    [self.view addSubview:horizontalScrollView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - ---------------------- DPHorizontalScrollViewDelegate
+- (NSInteger)numberOfColumnsInTableView{
+    return 10;
+}
+
+- (CGFloat)tableView:(DPHorizontalScrollView *)tableView widthForColumnAtIndex:(NSInteger)index{
+    return 150;
+}
+
+- (UIView *)tableView:(DPHorizontalScrollView *)tableView viewForColumnAtIndex:(NSInteger)index{
+    UIView *view = [tableView reusableView];
+    if (!view) {
+        view = [UIView new];
+    }
+    view.backgroundColor = index % 2 == 0 ? [UIColor cyanColor] : [UIColor orangeColor];
+    return view;
 }
 
 @end
